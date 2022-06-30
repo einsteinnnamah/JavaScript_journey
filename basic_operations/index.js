5159801f9a0eef27f935cf241e038aa558acf442
einsteinnnamah <einsteinnnamah@gmail.com>
2022-06-30 18:06:54 +0100

    more basic_operations

--- basic_operations/index.js
+++ basic_operations/index.js
@@ -9,29 +9,29 @@
 //*******************//
 
 //Greater than
-console.log(2 > 1) //true
+// console.log(2 > 1) //true
 
-//less than
-console.log(1 < 2) //true
+// //less than
+// console.log(1 < 2) //true
 
-//Greater than or equal to
-console.log(2>=2) //true
+// //Greater than or equal to
+// console.log(2>=2) //true
 
-//Less than or equal to
-console.log(2<=1) //false
+// //Less than or equal to
+// console.log(2<=1) //false
 
-//Equality
-console.log(2==1) //false
+// //Equality
+// console.log(2==1) //false
 
-//Inequality
-console.log(2!=1) //true
+// //Inequality
+// console.log(2!=1) //true
 
-//Strict equality
-console.log(2===1) //false
+// //Strict equality
+// console.log(2===1) //false
 
 
-//Assignment Operators
-let size = 10;
+// //Assignment Operators
+// let size = 10;
 // the assignment operator is used to assign a value to a variable
 //Do not confuse assignment operator with the equality operator
 
@@ -40,5 +40,41 @@ let size = 10;
 
 // Decrement operator
 // size -- / decreases the value of size by one
+
+//Addition assignment operator
+// size += 10 / adds 10 to size
+
+//Subtraction assignment operator
+// size -= 10 / subtracts 10 from size
+
+// Boolean Operators
+// *******************//
+
+// //Logical OR (||) /* This symbol is called the pipe symbol */
+// console.log(true || false) //true
+// console.log(false || false) //false
+// console.log(true || true) //true
+// console.log(false || true) //true
+
+// //Logical AND (&&) /* This symbol is called the ampersand symbol */
+// console.log(true && false) //false
+// console.log(false && false) //false
+// console.log(true && true) //true
+// console.log(false && true) //false
+
+// //Logical NOT (!) /* This symbol is called the tilde or exclamation mark symbol */
+// console.log(!true) //false
+// console.log(!false) //true
+
+
+// Precedence 
+// *******************//
+
+// let principal = 500;
+// let rate = 0.05;
+// let years = 5;
+
+// let simpleIntrest = (principal * rate * years) / 100;
+
 
 
