da054c69416df1fbff697de377de1461e1bff921
einsteinnnamah <einsteinnnamah@gmail.com>
2022-06-30 16:36:28 +0100

    conditional operators

---
+++ basic_operations/index.js
@@ -0,0 +1,32 @@
+
+
+
+//Comparism Operators
+// *******************//
+
+// Comparison Operators are operators used to compare two values e.g if two numbers are equal, if one number are greater than eachother
+// Note: all comparison operators return a boolean value i.e true or false
+//*******************//
+
+//Greater than
+console.log(2 > 1) //true
+
+//less than
+console.log(1 < 2) //true
+
+//Greater than or equal to
+console.log(2>=2) //true
+
+//Less than or equal to
+console.log(2<=1) //false
+
+//Equality
+console.log(2==1) //false
+
+//Inequality
+console.log(2!=1) //true
+
+//Strict equality
+console.log(2===1) //false
+
+
