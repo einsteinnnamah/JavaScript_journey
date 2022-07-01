01298e9203e9bb10dda374c923b8d546cd0c03f4
Einstein nnamah <einsteinnnamah@gmail.com>
2022-07-01 03:01:46 +0100

    conditional-if else statements

---
+++ Conditionals/index.js
@@ -0,0 +1,39 @@
+
+//Conditonals//
+/*************/
+
+//If Statement //
+let day = "monday"
+if (day == "monday") 
+console.log("Today is the day")
+//if ==> condition ==> if true ==> run code
+
+//Recommended way to write you readable if statement code
+let size = 23;
+
+if (size == 23) {
+    console.log("Error: not my spec!")
+}
+
+
+//If-else Statement
+/*********/
+let shopWidth = 40;
+
+if (shopWidth == 40) {
+    console.log("not my shop")
+} else{
+    console.log("i need to knw my shop width")
+}
+
+
+
+//multiply if-else
+/********/
+let showItems = "bananna"
+
+if (showItems == "bananna") {
+    console.log("item one")
+}else if (!showItems == "plantain") {
+    console.log("Missing Item")
+}
