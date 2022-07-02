adc9e87e2561af32f2a1ba777ca3b9c89b7849b7
einsteinnnamah <einsteinnnamah@gmail.com>
2022-07-02 22:08:19 +0100

    intro to conditional operator (?)

--- Conditionals/index.js
+++ Conditionals/index.js
@@ -1,39 +1,52 @@
 
-//Conditonals//
-/*************/
+// //Conditonals//
+// /*************/
 
-//If Statement //
-let day = "monday"
-if (day == "monday") 
-console.log("Today is the day")
-//if ==> condition ==> if true ==> run code
+// //If Statement //
+// let day = "monday"
+// if (day == "monday") 
+// console.log("Today is the day")
+// //if ==> condition ==> if true ==> run code
 
-//Recommended way to write you readable if statement code
-let size = 23;
+// //Recommended way to write you readable if statement code
+// let size = 23;
 
-if (size == 23) {
-    console.log("Error: not my spec!")
-}
-
-
-//If-else Statement
-/*********/
-let shopWidth = 40;
-
-if (shopWidth == 40) {
-    console.log("not my shop")
-} else{
-    console.log("i need to knw my shop width")
-}
+// if (size == 23) {
+//     console.log("Error: not my spec!")
+// }
 
 
+// //If-else Statement
+// /*********/
+// let shopWidth = 40;
 
-//multiply if-else
-/********/
-let showItems = "bananna"
+// if (shopWidth == 40) {
+//     console.log("not my shop")
+// } else{
+//     console.log("i need to knw my shop width")
+// }
 
-if (showItems == "bananna") {
-    console.log("item one")
-}else if (!showItems == "plantain") {
-    console.log("Missing Item")
-}
+
+
+// //multiply if-else
+// /********/
+// let showItems = "kunu"
+
+// if (showItems == "bananna") {
+
+//     console.log("What's the total cost")
+
+// } else if (showItems == "apple") {
+
+//     console.log("invalid item")
+// } else if (showItems == "orange") {
+
+//     console.log("invalid item")
+// } else (
+//     alert("invalid Shoping item")
+// )
+
+
+// Conditional operator (?:)
+// /********/
+// also called the ternary operator //
