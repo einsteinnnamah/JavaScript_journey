3042ac6db2750aa673d9f7b414c1acb3b1627a97
Einstein nnamah <einsteinnnamah@gmail.com>
2022-07-04 04:32:01 +0100

    Conditions in js

---
+++ index.js
@@ -0,0 +1,79 @@
+
+// //Conditonals//
+// /*************/
+
+// //If Statement //
+// let day = "monday"
+// if (day == "monday") 
+// console.log("Today is the day")
+// //if ==> condition ==> if true ==> run code
+
+// //Recommended way to write you readable if statement code
+// let size = 23;
+
+// if (size == 23) {
+//     console.log("Error: not my spec!")
+// }
+
+
+// //If-else Statement
+// /*********/
+// let shopWidth = 40;
+
+// if (shopWidth == 40) {
+//     console.log("not my shop")
+// } else{
+//     console.log("i need to knw my shop width")
+// }
+
+
+
+// //multiply if-else
+// /********/
+// let showItems = "kunu"
+
+// if (showItems == "bananna") {
+
+//     console.log("What's the total cost")
+
+// } else if (showItems == "apple") {
+
+//     console.log("invalid item")
+// } else if (showItems == "orange") {
+
+//     console.log("invalid item")
+// } else (
+//     alert("invalid Shoping item")
+// )
+
+
+// Conditional operator (?:)
+// /********/
+// also called the ternary operator //
+//Tenary operator operates on three values //
+let age = 20
+let canVote = ( age > 20) ? true : false
+console.log(canVote)
+
+let appleStock = 200
+let totalStock = (appleStock == 10) ? true : false
+console.log(totalStock)
+
+
+
+//Switch Statement //
+//********/
+
+let day = "Friday"
+
+switch (day) {
+    case 'Monday':
+        console.log("Today is Tuesday")
+        break;
+    case 'Wenesday':
+        console.log("Happy Birthday")
+        break;
+    default:
+        console.log("invalid User")
+        
+}
