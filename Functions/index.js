753089b949bcbe97a1efbcb1572b0a24abc54a59
Einstein nnamah <einsteinnnamah@gmail.com>
2022-07-04 05:45:37 +0100

    Intro to functions in JavaScript

---
+++ Functions/index.js
@@ -0,0 +1,29 @@
+// //FUnctions in JavaScript //
+
+// function sum () {
+//     result = 20 + 30
+//     console.log ( `This is the output ${result}`)
+// }
+
+// sum()
+
+
+// let count = 0;
+// //Function without  parameters //
+// function sumData () {
+//     let localCount = 1
+
+//     count = count + 1 //adding a global variable count
+//     localCount = localCount + 2 
+    
+//     console.log("New count value:", count)
+//     console.log("Local Count variable:", localCount)
+
+// }
+
+// sumData()
+
+
+//FUnctions with Parameters//
+/*******/
+
