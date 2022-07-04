117d16d7921595794d28f9ea991a879982cf0bea
Einstein nnamah <einsteinnnamah@gmail.com>
2022-07-04 05:21:58 +0100

    Loops in javaScript

---
+++ Loops/index.js
@@ -0,0 +1,57 @@
+//Loops //
+/****** */
+
+
+//While Loops ///
+// let i = 0;
+// while (i < 3) { // shows0, then 1, then 2
+//     console.log(i)
+//     i++;
+// }
+
+// //Do while loop //
+
+// let j = 0;
+
+// do {
+//     console.log(j)
+//     j++;
+  
+// }   while (j > 3)
+
+// //For Loops //
+// for ( i = 0; i < 20; i++ ) {
+//     console.log(i)
+// }
+
+
+
+
+// //For of Loop //
+// const iterable = [20, 30, 40, 50, 60];
+
+// for ( const value of iterable) {
+//     console.log("outlined value :", value);
+// }
+
+
+// const userAuth = ['kemi', 'shola', 'Gbemi'];
+
+// for (const char of userAuth) {
+//     console.log("userAuth values:" , char);
+// }
+
+
+
+//For in Loop//
+const userAuth = {
+ name: "shola",
+ age: 20,
+ city: "Lagos"
+
+}
+
+for (const key in userAuth) {
+    console.log(key)
+    console.log(`The key is: ${key} and the value is: ${userAuth[key]}`)
+}
