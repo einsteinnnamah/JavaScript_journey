b153558515412921824354164fba4a8f52a855e4
einsteinnnamah <einsteinnnamah@gmail.com>
2022-07-06 15:07:19 +0100

    scope and closures

--- DataTypes/index.js
+++ DataTypes/index.js
@@ -1,76 +1,76 @@
 
 
 
-//numbers
-let size_in_meters = 20 //interger
-let size_in_inches = 2.1023 //floating point number
-let maxSize = Infinity //positive Infinity
-let minSize = -Infinity //Negative infinity
-let not_a_number = NaN // Not a number (for operations that are not correct)
+// //numbers
+// let size_in_meters = 20 //interger
+// let size_in_inches = 2.1023 //floating point number
+// let maxSize = Infinity //positive Infinity
+// let minSize = -Infinity //Negative infinity
+// let not_a_number = NaN // Not a number (for operations that are not correct)
 
-//E.g
- let sum = 20 + 23.1; 
- console.log(sum) 
+// //E.g
+//  let sum = 20 + 23.1; 
+//  console.log(sum) 
 
- //BigInt
-let very_big_number = 8282342034234234141414124124124128580n;
-let another_big_number = BigInt(3124124124124214124)
+//  //BigInt
+// let very_big_number = 8282342034234234141414124124124128580n;
+// let another_big_number = BigInt(3124124124124214124)
 
-//Objects
-let user1 = {} //Recommended ways
+// //Objects
+// let user1 = {} //Recommended ways
 
-user1["name"] = "Azeez"
-user1["age"] = 20
-user1["activated"] = true
-user1["profile_settings"] = {
-    name: "shola", DOB: "20/22", 
-}
+// user1["name"] = "Azeez"
+// user1["age"] = 20
+// user1["activated"] = true
+// user1["profile_settings"] = {
+//     name: "shola", DOB: "20/22", 
+// }
 
-// console.log(user1)
+// // console.log(user1)
 
-let user3 = {
-    name: "Kenny John",
-    age: "20",
-    shop: "81/90",
-    color: "red",
-    "User Profile": {nextOfKin: "Shola Kumi", weight: "20pounds", Beneficary: "Shola Jonny"}
+// let user3 = {
+//     name: "Kenny John",
+//     age: "20",
+//     shop: "81/90",
+//     color: "red",
+//     "User Profile": {nextOfKin: "Shola Kumi", weight: "20pounds", Beneficary: "Shola Jonny"}
 
-}
-delete user3["User Profile"].weight
+// }
+// delete user3["User Profile"].weight
 
-// console.log(user3)
+// // console.log(user3)
 
-//get all the values in an object 
-
-
-// console.log(Object.values(user3))
-// console.log(Object.keys(user3))
-// console.log(typeof(user3.name))
+// //get all the values in an object 
 
 
-//More on Objects
-//Object.create()
-let userSelection = {
-    printUserProfile: function ()  {
-        console.log(`The selected user is Mr ${this.firstName} ${this.lastName}`)
-    }
-}
-
-const selectedUser = Object.create(userSelection);
-selectedUser.firstName = "Ayodeji";
-selectedUser.lastName = `Azeez`;
-selectedUser.printUserProfile()
+// // console.log(Object.values(user3))
+// // console.log(Object.keys(user3))
+// // console.log(typeof(user3.name))
 
 
+// //More on Objects
+// //Object.create()
+// let userSelection = {
+//     printUserProfile: function ()  {
+//         console.log(`The selected user is Mr ${this.firstName} ${this.lastName}`)
+//     }
+// }
+
+// const selectedUser = Object.create(userSelection);
+// selectedUser.firstName = "Ayodeji";
+// selectedUser.lastName = `Azeez`;
+// selectedUser.printUserProfile()
 
 
-//Strings
- let firstName = "Ayodeji"
- let lastName = 'Azeez'
- let fullName =`Mr ${firstName} ${lastName} `
-
-let message = "Hello my friends\n My name is Jonhson\n I am a Software engineer"
 
 
-// alert(message.includes("Software"))
+// //Strings
+//  let firstName = "Ayodeji"
+//  let lastName = 'Azeez'
+//  let fullName =`Mr ${firstName} ${lastName} `
+
+// let message = "Hello my friends\n My name is Jonhson\n I am a Software engineer"
+
+
+// // alert(message.includes("Software"))
 
---
+++ Scopes/index.js
@@ -0,0 +1,21 @@
+//Scopes in javaScript
+// Scope in javaScript refers to a context in which a code is executed, which determines the accessibility 
+//of various, object, variables and functions, etc.
+
+// For instance
+let size = 20;
+
+let userDoc = () => {
+    let firstName = "John";
+    console.log("This is a local variable", firstName);
+    console.log("This is a global variable", size);
+}
+
+userDoc()
+
+alert(size) // Works fine
+alert(firstName) //Throw an error, because name is not declared in scoope
+
+
+//Closures//
+//A closure is a function that has access to the parent scope, even after the parent function has closed.
